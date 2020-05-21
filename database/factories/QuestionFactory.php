38aa1cee71049be5875be4b3c142ed8570cbdec7
ngocvb <ngocvb@kaopiz.com>
2020-05-21 14:43:09 +0700

    Generate data for users and questions table via factory and seed

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(\App\Question::class, function (Faker $faker) {
+    return [
+        'title'=> rtrim($faker->sentence(rand(5,10)),"."),//rtrim de bo dau "." sau moi doan van
+        'body'=> $faker->paragraph(rand(3,7), true),//set true de xuat mot doan van hoan chinh thay vi array
+        'views'=>rand(0,10),
+        'answers'=>rand(0,10),
+        'votes'=>rand(-3,10),
+    ];
+});
