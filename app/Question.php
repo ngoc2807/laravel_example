c40121c968b7345ff8522c7844c6dbc1a7278ca3
ngocvb <ngocvb@kaopiz.com>
2020-05-24 10:20:14 +0700

    Validating and Saving the Question

--- app/Question.php
+++ app/Question.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    protected $filltable=['title','body'];
+    protected $fillable=['title','body'];
     public function user(){
         return $this->belongsTo(User::class);
     }
