cf870fb31a24b79414ea44666fec055d0adb47e0
ngocvb <ngocvb@kaopiz.com>
2020-05-25 15:05:06 +0700

    Design answers table and rename answers column to answers_count

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -10,7 +10,7 @@ $factory->define(\App\Question::class, function (Faker $faker) {
         'title'=> rtrim($faker->sentence(rand(5,10)),"."),//rtrim de bo dau "." sau moi doan van
         'body'=> $faker->paragraph(rand(3,7), true),//set true de xuat mot doan van hoan chinh thay vi array
         'views'=>rand(0,10),
-        'answers'=>rand(0,10),
+        'answers_count'=>rand(0,10),
         'votes'=>rand(-3,10),
     ];
 });
